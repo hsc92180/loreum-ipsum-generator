99b4a5857914291d8dde36f25dadddffbc59a7e4
hsc92180 <68774484+hsc92180@users.noreply.github.com>
2024-03-11 23:44:53 +0530

    add loreum project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import data from "./data.json";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  let [totalPara, setTotalPara] = useState(0);
+  let [paraData, setParaData] = useState([]);
+
+  const GenerateData = () => {
+    if (totalPara < 1) {
+      alert("Please enter a number greater than zero.");
+      return;
+    } else if (totalPara > 9) {
+      alert("You can only generate up to 9 paragraphs at once.");
+      return;
+    }
+    for (let i = 0; i < totalPara; i++) {
+      setParaData((prevData) => {
+        if (i === 0) {
+          return [data[i]];
+        }
+        return [...prevData, data[i]];
+      });
+    }
+  };
+
+  const sharePara = (para) => {
+    navigator.share({
+      title: "Share Paragraph",
+      text: para,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h2>TIRED OF BORING LOREM IPSUM?</h2>
+      <div className="form">
+        <label htmlFor="para">Paragraphs</label>
+        <input
+          type="number"
+          placeholder="Paragraphs"
+          id="para"
+          min={1}
+          step={1}
+          value={totalPara}
+          onChange={(e) => setTotalPara(e.target.value)}
+          max={9}
+        />
+        <button title="Generate" onClick={GenerateData}>
+          Generate
+        </button>
+      </div>
+
+      <div className="para-container">
+        {paraData.map((item, index) => {
+          return (
+            <div key={index} className="para">
+              <p>{item}</p>
+            </div>
+          );
+        })}
+      </div>
+    </main>
   );
 }
 
